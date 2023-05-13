75b34f036a06e7caea28e562be4c4683a0b0f1bc
Luis-Paulo <luis.andreassi@gmail.com>
2023-05-13 17:08:37 -0300

    Commit1-Sabado13.05

---
+++ Umfg.Programacaoiv2023.Primeira.Api/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Umfg.Programacaoiv2023.RestFulProduto.Api
+{
+    public sealed class Produto
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; private set; }
+
+        [JsonPropertyName("descricao")]
+        public string Descricao { get; set; }
+
+        [JsonPropertyName("codigoBarra")]
+        public string CodigoDeBarras { get; set; }
+
+        [JsonPropertyName("valor")]
+        public float Valor { get; set; }
+
+
+        private Produto() { }
+
+        public Produto(string descricao, string codigoDeBarras, float valor)
+        {
+            Id = Guid.NewGuid();
+            Descricao = descricao;
+            CodigoDeBarras = codigoDeBarras;
+            Valor = valor;
+        }
+    }
+}
