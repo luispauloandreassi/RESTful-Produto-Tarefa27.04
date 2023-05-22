c3e291e8ebc51c9a3c897827d1fb4f9710015eda
Luis Paulo <luis.andreassi@gmail.com>
2023-05-21 21:09:17 -0300

    Commit Produto

---
+++ Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ExercícioAPI_RESTful_Produto__
+{
+    internal class Produto
+    {
+
+        [JsonPropertyName("id")]
+        public Guid Id { get; private set; }
+
+        [JsonPropertyName("descricao")]
+        public string Descricao { get; set; }
+
+        [JsonPropertyName("codigoBarra")]
+        public string CodigoDeBarras { get; set; }
+
+        [JsonPropertyName("valor")]
+        public float Valor { get; set; }
+
+
+        private Produto() { }
+
+        public Produto(string descricao, string codigoDeBarras, float valor)
+        {
+            Id = Guid.NewGuid();
+            Descricao = descricao;
+            CodigoDeBarras = codigoDeBarras;
+            Valor = valor;
+        }
+
+
+    }
+}
